e8e7ab30d9b1bff36d80fa04192792684e7cf741
heremit <andre.myszko@gmail.com>
2024-05-28 10:44:19 -0300

    add more currency types to api services

--- core/services/currency_service.py
+++ core/services/currency_service.py
@@ -6,7 +6,10 @@ def get_coingecko_values():
         # Make a GET request to the CoinGecko API
         url = "https://api.coingecko.com/api/v3/simple/price"
         params = {
-            "ids": "bitcoin,ethereum",
+            "ids": "bitcoin,ethereum,litecoin,ripple,bitcoin-cash,cardano,"
+                   "polkadot,stellar,chainlink,binancecoin,tether,dogecoin,"
+                   "solana,uniswap,vechain,monero,eos,aave,cosmos,tron,"
+                   "tezos,iota,neo,dash,zcash",
             "vs_currencies": "usd"
         }
         response = requests.get(url, params=params)
@@ -16,17 +19,47 @@ def get_coingecko_values():
         # USD static value
         usd_rate = 1.00
 
-        # Extract exchange rates between BITCOIN and USD options[BRL and EUR]
-        btc_usd_rate = usd_rate / data['bitcoin']['usd']
-
-        # Extract exchange rates between ETHERIUM and USD options[BRL and EUR]
-        eth_usd_rate = usd_rate / data['ethereum']['usd']
-
-        return {
-            "USD": '{:.8f}'.format(usd_rate),
-            "BTC": '{:.8f}'.format(btc_usd_rate),
-            "ETH": '{:.8f}'.format(eth_usd_rate)
+        # Dictionary to map cryptocurrency IDs to their symbols
+        crypto_symbols = {
+            'bitcoin': 'BTC', 
+            'ethereum': 'ETH', 
+            'litecoin': 'LTC', 
+            'ripple': 'XRP',
+            'bitcoin-cash': 'BCH', 
+            'cardano': 'ADA', 
+            'polkadot': 'DOT', 
+            'stellar': 'XLM',
+            'chainlink': 'LINK', 
+            'binancecoin': 'BNB', 
+            'tether': 'USDT', 
+            'dogecoin': 'DOGE',
+            'solana': 'SOL', 
+            'uniswap': 'UNI', 
+            'vechain': 'VET', 
+            'monero': 'XMR', 
+            'eos': 'EOS',
+            'aave': 'AAVE', 
+            'cosmos': 'ATOM', 
+            'tron': 'TRX', 
+            'tezos': 'XTZ', 
+            'iota': 'MIOTA',
+            'neo': 'NEO', 
+            'dash': 'DASH', 
+            'zcash': 'ZEC'
         }
+
+        # Initialize dictionary to store exchange rates
+        exchange_rates = {"USD": '{:.8f}'.format(usd_rate)}
+
+        # Iterate through the crypto_symbols dictionary and extract their rates
+        for crypto_id, symbol in crypto_symbols.items():
+            try:
+                rate = usd_rate / data[crypto_id]['usd']
+                exchange_rates[symbol] = '{:.8f}'.format(rate)
+            except KeyError:
+                print(f"Error: No rate found for {crypto_id}")
+
+        return exchange_rates
     except requests.RequestException as e:
         print(f"Error fetching CoinGecko values: {e}")
         return None
@@ -40,13 +73,14 @@ def get_open_exchange_rates_values():
         response.raise_for_status()  # Raise an exception for HTTP errors
         data = response.json()
 
-        brl_rate = data['rates']['BRL']
-        eur_rate = data['rates']['EUR']
+        # Extract rates for desired currencies
+        currencies = ['BRL', 'EUR', 'GBP', 'JPY',
+                      'AUD', 'CAD', 'CHF', 'CNY', 'SEK', 'NZD']
+        exchange_rates = {currency: '{:.8f}'.format(
+            data['rates'][currency]) for currency in currencies}
 
-        return {
-            "BRL": '{:.8f}'.format(brl_rate),
-            "EUR": '{:.8f}'.format(eur_rate)
-        }
+        return exchange_rates
+
     except requests.RequestException as e:
         print(f"Error fetching Open Exchange Rates values: {e}")
         return None
